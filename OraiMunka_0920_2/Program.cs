a7d41f3ba6e390bff177454a151c85230f4a876b
Kotai <kotai@info.com>
2019-09-20 12:09:48 +0200

    Alap elkeszitve

--- OraiMunka_0920_2/Program.cs
+++ OraiMunka_0920_2/Program.cs
@@ -8,8 +8,34 @@ namespace OraiMunka_0920_2
 {
     class Program
     {
+        static List<int> lista;
+        static void szamBekeres() {
+            int index = 0;
+            lista = new List<int>(); 
+            while (index < 10) {
+                Console.Write("Kérem adjon megy egy számot: ");
+                int szam = Convert.ToInt32(Console.ReadLine());
+                lista.Add(szam);
+                index++;
+            }
+        }
+
+        static void szamKiiras() {
+            for (int i = 0; i < lista.Count-1; i++)
+            {
+                Console.Write(lista[i] + ", ");
+            }
+            Console.WriteLine(lista[lista.Count-1]);
+        }
+
         static void Main(string[] args)
         {
+            szamBekeres();
+            szamKiiras();
+
+
+
+            Console.ReadKey();
         }
     }
 }
